d7d57bb46d20189e47ab6bd34cd6c9815e5e3484
saberkan <salahddine.aberkan@gmail.com>
2020-08-20 14:36:05 +0200

    Adding all files

---
+++ src/test/java/tk/saberkan/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package tk.saberkan;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
